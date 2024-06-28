cf8e10f33c0543a0e6b1c3d18fd08e2ac79d86bb
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2024-06-27 22:44:51 -0400

    add express

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import * as express from 'express';
+
+export async function createNestExpressApp(server: any) {
+  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+  await app.init();
+}
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  const server = express();
+  await createNestExpressApp(server);
+  await server.listen(3000);
 }
 bootstrap();
--- vercel-server.js
+++ vercel-server.js
@@ -1,6 +1,15 @@
-import { createServer } from 'vercel-node-server';
-import { join } from 'path';
-const appPath = join(__dirname, 'dist/main');
-const app = require(appPath);
+const { createServer } = require('vercel-node-server');
+const { join } = require('path');
+const express = require('express');
 
-module.exports = createServer(app);
+const appPath = join(__dirname, 'dist/main');
+const appModule = require(appPath);
+const server = express();
+
+async function bootstrap() {
+  await appModule.createNestExpressApp(server);
+}
+
+bootstrap();
+
+module.exports = createServer(server);
