8b4e22660dd5f456c774e1126369bfc2d44b1e55
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2024-06-27 21:49:12 -0400

    Develop webhook

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Post, Body, Headers, HttpCode } from '@nestjs/common';
 
-@Controller()
-export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get('/wena')
-  getHello(): string {
-    return 'Hola';
+@Controller('webhook')
+export class WebhookController {
+  @Post()
+  @HttpCode(200) // Respond with a 200 status code
+  handleWebhook(
+    @Body() body: any, // The payload of the webhook
+    @Headers() headers: any, // Headers sent with the webhook
+  ): void {
+    console.log('Received webhook:', body);
+    console.log('Headers:', headers);
+    // Process the webhook data here...
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { WebhookController } from './app.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [WebhookController],
 })
 export class AppModule {}
