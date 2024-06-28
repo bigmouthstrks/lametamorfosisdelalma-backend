74966bf83a3d86ac919f655fa83ae583e94bd07c
Benjamin Caceres <benjamin.caceres.ra@gmail.com>
2024-06-28 17:45:30 -0400

    Add Heroku config

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ExpressAdapter } from '@nestjs/platform-express';
-
 const express = require('express');
 
-export async function createNestExpressApp() {
+async function bootstrap() {
   const server = express();
   const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
-  await app.init();
-  return server;
-}
-
-async function bootstrap() {
-  const server = await createNestExpressApp();
-  await server.listen(3000);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
